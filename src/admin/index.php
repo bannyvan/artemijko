9872c9832b6ab85c178fa5e704677467dcb8d51b
bannyvan <bannyvan@ya.ru>
2025-07-18 23:35:11 +0300

    Add admin login system

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['admin_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
 // Placeholder for admin panel
 // Future implementation will use Bootstrap and AdminLTE
 
